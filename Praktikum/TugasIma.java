4a10ff473be31d551bd189bab9c43ddf2a7d3620
unknown <you@example.com>
2021-10-03 17:53:13 +0700

    TugasIma.java

---
+++ Praktikum/TugasIma.java
@@ -0,0 +1,11 @@
+public class TugasIma {
+
+	public static void main (String[] args) {
+		System.out.println ("Nama	:Nursilman Raja Saputra Azhary");
+		System.out.println ("NIM	:6706213151");
+		System.out.println ("Kelas	:D3RPLA4505");
+		System.out.println ("IPK	:4.00");
+	}
+}
+		
+		
